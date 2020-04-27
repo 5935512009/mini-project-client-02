2319ab70415b54077b48dd5a0c95c00ead724ca9
5935512009 <Pasut2541@hotmail.com>
2020-04-27 03:35:33 -0500

    First

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const Config = {
+    apiKey: "AIzaSyBHGraVugqeKm9T9uweZT-GYoFvkCD0MBQ",
+    authDomain: "miniproject-cafe1.firebaseapp.com",
+    databaseURL: "https://miniproject-cafe1.firebaseio.com",
+    projectId: "miniproject-cafe1",
+    storageBucket: "miniproject-cafe1.appspot.com",
+    messagingSenderId: "979621584658",
+    appId: "1:979621584658:web:4930a3972237f81dc85dcb",
+    measurementId: "G-VLBXEBY724"
+  };
+
+  export default Config
